efd638cc9c519c02999904df9474fef717773e62
DarkPhantom <l2kwstasl2@gmail.com>
2020-12-03 18:21:31 +0200

    Day2 part2 solution

--- src/day2.rs
+++ src/day2.rs
@@ -1,11 +1,17 @@
 pub fn day2_part1(input: &str) -> i32 {
     let count = input.lines().fold(0, |acc, line| {
-        let line = line.split(':').collect::<Vec<&str>>();
-        let (requirements, password) = (line[0], line[1]);
-        let requirements = requirements.split_ascii_whitespace().collect::<Vec<&str>>();
-        let (numbers, letter) = (requirements[0], requirements[1]);
-        let numbers = numbers.split('-').collect::<Vec<&str>>();
-        let (min, max): (usize, usize) = (numbers[0].parse().unwrap(), numbers[1].parse().unwrap());
+        let (requirements, password) = {
+            let line = line.split(':').collect::<Vec<&str>>();
+            (line[0], line[1])
+        };
+        let (numbers, letter) = {
+            let requirements = requirements.split_ascii_whitespace().collect::<Vec<&str>>();
+            (requirements[0], requirements[1])
+        };
+        let (min, max) = {
+            let numbers = numbers.split('-').collect::<Vec<&str>>();
+            (numbers[0].parse().unwrap(), numbers[1].parse().unwrap())
+        };
         let count = password
             .chars()
             .into_iter()
@@ -22,20 +28,29 @@ pub fn day2_part1(input: &str) -> i32 {
 
 pub fn day2_part2(input: &str) -> i32 {
     let count = input.lines().fold(0, |acc, line| {
-        let line = line.split(':').collect::<Vec<&str>>();
-        let (requirements, password) = (line[0], line[1]);
-        let requirements = requirements.split_ascii_whitespace().collect::<Vec<&str>>();
-        let (numbers, letter) = (requirements[0], requirements[1]);
-        let numbers = numbers.split('-').collect::<Vec<&str>>();
-        let (min, max): (usize, usize) = (numbers[0].parse().unwrap(), numbers[1].parse().unwrap());
-        let count = password
-            .chars()
-            .into_iter()
-            .filter(|character| *character == letter.chars().nth(0).unwrap())
-            .count();
-        if count >= min && count <= max {
+        let (requirements, password) = {
+            let line = line.split(':').collect::<Vec<&str>>();
+            (line[0], line[1].trim())
+        };
+        let (numbers, letter) = {
+            let requirements = requirements.split_ascii_whitespace().collect::<Vec<&str>>();
+            (requirements[0], requirements[1])
+        };
+        let (first, second): (usize, usize) = {
+            let numbers = numbers.split('-').collect::<Vec<&str>>();
+            (numbers[0].parse().unwrap(), numbers[1].parse().unwrap())
+        };
+        let letter = letter.chars().nth(0).unwrap();
+        let (pass_first, pass_second) = {
+            (password.chars().nth(first - 1).unwrap(), password.chars().nth(second - 1).unwrap())
+        };
+        if pass_first == letter && pass_second != letter {
             return acc + 1;
-        } else {
+        }
+        else if pass_first != letter && pass_second == letter {
+            return acc + 1;
+        }
+        else {
             return acc;
         }
     });
